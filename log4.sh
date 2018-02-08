08294beede0712f59657943e389713b6d73c6c62
cainesmckoy <caine12393@yahoo.com>
2018-02-07 22:05:28 -0500

    Update log4.sh

--- log4.sh
+++ log4.sh
@@ -5,7 +5,7 @@ sudo rm info
 cd
 sudo rm -rf 115
 sudo mkdir 115
-sudo chmod 700 115
+sudo chmod 600 115
 sudo chown caine:caine 115
 echo "THESE FILES HAVE BEEN EDITED" > 115/info
 sudo find /etc /mnt /home . -mmin -30 -type f -print >> 115/info
