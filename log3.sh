060dbb0acc304ff90468e9e502c5c207941dd52e
cainesmckoy <caine12393@yahoo.com>
2018-02-16 11:30:27 -0500

    Update log3.sh

--- log3.sh
+++ log3.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # THIS IS A SCRIPT TO PULL IMPORTANT INFO FROM LOG FILES
+#COPY SCRIPT AS log3-1.sh
+#REMOVE -1 in log3-1.sh
+
+sudo pkill -9 log3-1.sh
 NOW=$( date '+%s' )
 REF=$(( $NOW - 15*60 ))
 
@@ -13,4 +17,4 @@ while read MONTH DAY HMS A4 ; do
 
 done < /var/lib/bind/query.log | grep 'query'
 sleep 15
-sudo ./log3.sh
+sudo ./log3-1.sh
